88b65d951dd9386df5154ed83dccc6cf3c3b5b8e
MuhammadAbdulSalam <abdulsalamch32@gmail.com>
2022-10-10 21:56:39 +0100

    updated pattern setup

---
+++ app/src/main/java/com/freeagent/testapp/ui/fragments/rateslistfragment/adapters/RatesListAdapter.kt
@@ -0,0 +1,23 @@
+package com.freeagent.testapp.ui.fragments.rateslistfragment.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+//class RatesListAdapter : RecyclerView.Adapter<RatesListAdapter.ViewHolder>() {
+
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+//        ViewHolder(ItemViewCurrencyBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+//
+//    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+//
+//    }
+//
+//    override fun getItemCount(): Int {
+//        return 0 //TODO: Replace this with the actual count
+//    }
+//
+//    class ViewHolder(private val binding: ItemViewCurrencyBinding) : RecyclerView.ViewHolder(binding.root) {
+//
+//    }
+//}

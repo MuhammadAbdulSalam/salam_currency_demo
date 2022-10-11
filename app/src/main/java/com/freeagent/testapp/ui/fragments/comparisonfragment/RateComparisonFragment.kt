9ed5f1bbe8e2cb626ba12e27605ece9eb32c6e52
MuhammadAbdulSalam <abdulsalamch32@gmail.com>
2022-10-11 22:18:30 +0100

    updated dummy table for conversion

--- app/src/main/java/com/freeagent/testapp/ui/fragments/comparisonfragment/RateComparisonFragment.kt
+++ app/src/main/java/com/freeagent/testapp/ui/fragments/comparisonfragment/RateComparisonFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import com.freeagent.testapp.api.data.ConversionHistory
+import com.freeagent.testapp.api.data.CurrencyRates
 import com.freeagent.testapp.databinding.FragmentRateComparisonBinding
 import com.freeagent.testapp.utils.AppCurrency
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,6 +22,22 @@ class RateComparisonFragment: Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentRateComparisonBinding.inflate(inflater, container, false)
+
+
+        val conversionRateOne = CurrencyRates(AppCurrency.EUR, "200")
+        val conversionRateTwo = CurrencyRates(AppCurrency.GBP, "100")
+
+
+        val dayOne = ConversionHistory.DatasetByDay("20/20/200", conversionRateOne , conversionRateTwo)
+        val dayTwo = ConversionHistory.DatasetByDay("21/20/200", conversionRateOne , conversionRateTwo)
+        val dayThree = ConversionHistory.DatasetByDay("22/20/200", conversionRateOne , conversionRateTwo)
+        val dayFour = ConversionHistory.DatasetByDay("23/20/200", conversionRateOne , conversionRateTwo)
+        val dayFive = ConversionHistory.DatasetByDay("24/20/200", conversionRateOne , conversionRateTwo)
+
+        val conversionHistory = ConversionHistory(dayOne, dayTwo, dayThree, dayFour, dayFive)
+
+        binding.dataset = conversionHistory
+
         return binding.root
     }
 
